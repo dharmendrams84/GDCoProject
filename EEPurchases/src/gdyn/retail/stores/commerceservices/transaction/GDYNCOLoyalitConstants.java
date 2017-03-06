3034c2005f88d87a5ab928b8640ce0e68ac2ae15
Ignitiv021 <Ignitiv021@Ignitiv021-PC>
2017-03-06 15:09:11 +0530

    COMMING GDYNCOLOYALITYCONSTANTS.JAVA

--- EEPurchases/src/gdyn/retail/stores/commerceservices/transaction/GDYNCOLoyalitConstants.java
+++ EEPurchases/src/gdyn/retail/stores/commerceservices/transaction/GDYNCOLoyalitConstants.java
@@ -21,13 +21,36 @@ public class GDYNCOLoyalitConstants implements Serializable{
 	}
 	
 	public static boolean isValidLoyaltyId(String loyaltyId) {
-
+		if(loyaltyId.startsWith("0")||loyaltyId.startsWith("1")){
+			return false;
+		}
 		try {
 			Long.parseLong(loyaltyId);
 		} catch (NumberFormatException e) {
 			return false;
 		}
-
+		
 		return true;
 	}
+
+	public static boolean validateLuhnAlgorithm(String ccNumber)
+    {
+            int sum = 0;
+            boolean alternate = false;
+            for (int i = ccNumber.length() - 1; i >= 0; i--)
+            {
+                    int n = Integer.parseInt(ccNumber.substring(i, i + 1));
+                    if (alternate)
+                    {
+                            n *= 2;
+                            if (n > 9)
+                            {
+                                    n = (n % 10) + 1;
+                            }
+                    }
+                    sum += n;
+                    alternate = !alternate;
+            }
+            return (sum % 10 == 0);
+    }
 }
